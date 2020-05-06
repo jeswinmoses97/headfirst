8cd171d71a842c165177a2bf3303f8433a08d8d8
Jeswin <mosesjeswin@gmail.com>
2020-05-06 21:15:03 +0530

    simple cal

--- chapter1/src/com/test/Ifdemo.java
+++ chapter1/src/com/test/Ifdemo.java
@@ -3,18 +3,25 @@ package com.test;
 import java.util.Scanner;
 
 public class Ifdemo {
-    public static void main(String[] args)
-    {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter age" );
-        int age =scanner.nextInt();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Value for a ");
+        int a = scanner.nextInt();
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Enter value for b");
+        int b = scanner1.nextInt();
 
-        if (age>=18){
-            System.out.println("You are major");
-        }
-        else {
-            System.out.println("You are mot allowed to vote");
-        }
-        System.out.println("Thank you");
-        }
+        int add = a+b;
+        System.out.println("Add =" +add);
+
+        int sub = a-b;
+        System.out.println("Sub =" +sub);
+
+        int mul = a*b;
+        System.out.println("Mul =" +mul);
+
+        int div = a/b;
+        System.out.println("Div =" +div);
+
+    }
 }
