d981b4499c4f8545bdb9e03befe9583fe6304826
Jeswin <mosesjeswin@gmail.com>
2020-05-06 22:02:07 +0530

    simple cal

---
+++ chapter1/src/com/test/Calci.java
@@ -0,0 +1,42 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Calci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Value for a ");
+        int a = scanner.nextInt();
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Enter value for b");
+        int b = scanner1.nextInt();
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Enter operation");
+        String opp = scanner2.next();
+        {
+            int c;
+
+            switch (opp) {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    c = a / b;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + opp);
+            }
+
+
+            System.out.println("result = "+c);
+        }
+
+    }
+}
