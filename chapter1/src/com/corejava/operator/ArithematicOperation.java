2e7ca5f621d5ac2b225aafcc7157027647403cd4
Jeswin <mosesjeswin@gmail.com>
2020-07-10 10:14:00 +0530

    Operators1

--- chapter1/src/com/corejava/operator/ArithematicOperation.java
+++ chapter1/src/com/corejava/operator/ArithematicOperation.java
@@ -2,6 +2,13 @@ package com.corejava.operator;
 
 public class ArithematicOperation {
     public static void main (String[] agrs){
-        
+        int i,j;
+        for (j=1;j<101;j++){
+            System.out.println(j);
+        }
+        for (i=99;i>0;i--)
+        {
+            System.out.println(i);
+        }
     }
 }
