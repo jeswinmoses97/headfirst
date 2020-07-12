7794fab8d0f18e52294848b3938bb7b748ade275
Jeswin <mosesjeswin@gmail.com>
2020-07-12 14:01:16 +0530

    sum of prime num

--- chapter1/src/com/corejava/operator/SumPrimeNum.java
+++ chapter1/src/com/corejava/operator/SumPrimeNum.java
@@ -2,30 +2,30 @@ package com.corejava.operator;
 
 public class SumPrimeNum {
     public static void main(String[] args) {
-            long i =0;
-            long num =0;
-            String primeSum="";
+            int i =0;
+            int num =0;
             String  primeNumbers = "";
+            int sumPrime =0;
 
             for (i = 1; i <= 50; i++)
             {
-                long counter=0;
+                long j=0;
                 for(num =i; num>=1; num--)
                 {
                     if(i%num==0)
                     {
-                        counter = counter + 1;
+                        j = j + 1;
                     }
                 }
-                if (counter ==2)
+                if (j ==2)
                 {
-
-                    primeNumbers = primeNumbers + i + " ";
-
+                    primeNumbers = primeNumbers + i+" " ;
+                    sumPrime=sumPrime+i;
                 }
             }
             System.out.println("Prime numbers from 1 to 50 are :");
             System.out.println(primeNumbers);
+        System.out.println("sum of prime numbers from 1 to 50 : "+sumPrime);
 
         }
     }
